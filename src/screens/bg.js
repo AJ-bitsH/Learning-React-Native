2b74c094c9c76ebf5b884abef150653d85ec63b0
DelhiBoi <52867282+DelhiBoi@users.noreply.github.com>
2020-10-19 15:53:56 +0530

    changes

--- src/screens/bg.js
+++ src/screens/bg.js
@@ -6,17 +6,27 @@ import {
   View,
   TouchableOpacity,
 } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const App = () => (
   <View style={styles.container}>
-    <ImageBackground source={require("./assets/apt.jpeg")} style={styles.image}>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.text}> Register </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.text}> Enter </Text>
-      </TouchableOpacity>
-    </ImageBackground>
+    <LinearGradient
+      colors={["#09203f", "#537895"]}
+      start={[0.9, 0.9]}
+      style={styles.linearGradient}
+    >
+      <ImageBackground
+        source={require("../../assets/apt2.jpeg")}
+        style={styles.image}
+      >
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}> Register </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}> Enter </Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </LinearGradient>
   </View>
 );
 
@@ -26,9 +36,10 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "purple",
   },
   button: {
-    flex: 1,
+    //flex: 1,
     width: 250,
     height: 44,
     padding: 10,
@@ -38,18 +49,26 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     backgroundColor: "rebeccapurple",
     justifyContent: "center",
+    top: 50,
   },
   image: {
-    flex: 1,
-    resizeMode: "cover",
+    alignItems: "center",
     justifyContent: "center",
-    height: 1280,
-    width: 720,
+    height: 400,
+    width: 400,
   },
   text: {
     color: "grey",
     fontSize: 30,
     fontWeight: "bold",
+    alignSelf: "center",
+  },
+  linearGradient: {
+    width: "100%",
+    height: "100%",
+    opacity: 0.95,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
